5ca355c68bbe41ca9715cd617580d1d4795ab798
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-09-24 20:29:17 -0600

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import {Router, Route} from 'react-router';
+
+class Home extends Component {
+  render(){
+    return (<h1>Hi</h1>);
+  }
+}
+
+render(
+  <Router>
+    {/*Each routes is defined with Route*/}
+    <Route path='/' component={Home}/>
+  </Router>,
+  document.getElementById('container')
+);
